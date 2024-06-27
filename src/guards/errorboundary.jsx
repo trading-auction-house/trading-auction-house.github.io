8d9c8a973db78ddf403f361d7ff3949c700b7c04
atanas-zahariev <atanaszaharieff@abv.bg>
2024-06-27 17:29:36 +0300

    Initializing errorboundary.

---
+++ src/guards/errorboundary.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Spinner from '../components/common/Spinner';
+
+export default class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false, redirect: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.log(error, errorInfo);
+    }
+
+    componentDidUpdate() {
+        if (this.state.hasError) {
+            setTimeout(() => this.setState({ redirect: true }), 2000);
+        }
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return window.location.reload();;
+        }
+        if (this.state.hasError) {
+            localStorage.clear();
+            return (
+                <>
+                    <h1>It seems to be a problem please waith 2 seconds and login again.</h1>
+                    <Spinner />
+                </>
+            );
+        }
+
+        return this.props.children;
+    }
+}
