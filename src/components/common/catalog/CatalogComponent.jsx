2464087e1b2365c9cb33143d3d7d6c77bde78316
atanas-zahariev <atanaszaharieff@abv.bg>
2024-07-20 17:52:11 +0300

    Add Spinner.

--- src/components/common/catalog/CatalogComponent.jsx
+++ src/components/common/catalog/CatalogComponent.jsx
@@ -6,13 +6,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Item } from './ItemComponent';
 
 import { cleanAuthError, selectAuthError } from '../../../slices/authSlice';
-import { cleanErrorFromCatalog, selectItems, selectItemsError } from '../../../slices/itemsSlice';
+import { cleanErrorFromCatalog, getItems, selectItems, selectItemsError, selectItemsStatus } from '../../../slices/itemsSlice';
+
+import Spinner from '../Spinner';
 
 export default function Catalog() {
-    const authError = useSelector(selectAuthError);
-    const itemsError = useSelector(selectItemsError);
     const dispatch = useDispatch();
+
+    const authError = useSelector(selectAuthError);
+
+    const itemsError = useSelector(selectItemsError);
+
     const items = useSelector(state => selectItems(state));
+
+    const status = useSelector(selectItemsStatus);
+
+    const fetchItemsRequest = status === 'fetchItemStarted'
 
     useEffect(() => {
         if (authError) {
@@ -21,24 +30,28 @@ export default function Catalog() {
         if (itemsError) {
             dispatch(cleanErrorFromCatalog());
         }
+        dispatch(getItems())
         // eslint-disable-next-line
     }, []);
 
     return (
-        <section id="catalog-section" className="spaced">
+        <div>
+            {!fetchItemsRequest &&<section id="catalog-section" className="spaced">
 
-            {items?.length > 0 ?
-                <ul className="catalog cards">
-                    {items.map(x => <Item key={x.id} {...x} />)}
-                </ul> :
-                <div className="item pad-large align-center">
-                    <p>Nothing has been listed yet. Be the first!</p>
-                    <div>
-                        <Link className="action" to="/create">Publish Auction</Link>
+                {items?.length > 0 ?
+                    <ul className="catalog cards">
+                        {items.map(x => <Item key={x.id} {...x} />)}
+                    </ul> :
+                    <div className="item pad-large align-center">
+                        <p>Nothing has been listed yet. Be the first!</p>
+                        <div>
+                            <Link className="action" to="/create">Publish Auction</Link>
+                        </div>
                     </div>
-                </div>
-            }
+                }
 
-        </section>
+            </section>}
+            {fetchItemsRequest && <Spinner />}
+        </div>
     );
 }
