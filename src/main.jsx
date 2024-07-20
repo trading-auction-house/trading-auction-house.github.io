9620cf3594cf31b7708ccb7a88859f518a601ec7
atanas-zahariev <atanaszaharieff@abv.bg>
2024-07-20 17:48:17 +0300

    Remove initial items fetching.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { HashRouter as Router} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 import App from './App';
@@ -10,32 +10,25 @@ import App from './App';
 import './index.css';
 
 import { store } from './app/store';
-import { getItems } from './slices/itemsSlice';
 
+const container = document.getElementById('root');
 
-async function fetchItemBeforeRender() {
+if (container) {
+  const root = createRoot(container);
 
-  await store.dispatch(getItems());
-
-  const container = document.getElementById('root');
-
-  if (container) {
-    const root = createRoot(container);
-
-    root.render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <Router>
-            <App />
-          </Router>
-        </Provider>
-      </React.StrictMode>,
-    );
-  } else {
-    throw new Error(
-      'Root element with ID \'root\' was not found in the document. Ensure there is a corresponding HTML element with the ID \'root\' in your HTML file.',
-    );
-  }
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </React.StrictMode>,
+  );
+} else {
+  throw new Error(
+    'Root element with ID \'root\' was not found in the document. Ensure there is a corresponding HTML element with the ID \'root\' in your HTML file.',
+  );
 }
 
-fetchItemBeforeRender();
+
