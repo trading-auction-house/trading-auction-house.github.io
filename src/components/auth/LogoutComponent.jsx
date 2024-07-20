9ef190dd38788b5e0e425b76b76e58bcb8e9ca60
atanas-zahariev <atanaszaharieff@abv.bg>
2024-07-20 17:51:20 +0300

    Add Spinner and clearClosedOffers.

--- src/components/auth/LogoutComponent.jsx
+++ src/components/auth/LogoutComponent.jsx
@@ -3,29 +3,39 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { logoutUser, selectUser } from '../../slices/authSlice';
-import { clearUserFromCatalog } from '../../slices/itemsSlice';
+import { logoutUser, selectAuthStatus, selectUser } from '../../slices/authSlice';
+import { clearUserFromCatalog, clearClosedOffers } from '../../slices/itemsSlice';
 
+import Spinner from '../common/Spinner';
 
 export default function Logout() {
+    const status = useSelector(selectAuthStatus);
     const user = Object.entries(useSelector(selectUser).entities);
-    
+
     const dispatch = useDispatch();
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        async function fetchData() {
-            if(user[0]){
+        async function wait() {
+            await logout();
+            navigate('/');
+        }
+
+        async function logout() {
+            if (user[0]) {
                 await dispatch(logoutUser(user[0][0]));
-            }else{
+            } else {
                 dispatch(logoutUser());
             }
             dispatch(clearUserFromCatalog());
+            dispatch(clearClosedOffers());
         }
-        
-        fetchData();
-        navigate('/');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+        wait()
     }, []);
+
+    if(status === 'logoutStarted'){
+        return <Spinner />
+    }
 }
