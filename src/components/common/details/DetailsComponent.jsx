640df23b3046e74acc8a7040ec9121d94ecec1c0
Verhus <atanaszaharieff@abv.bg>
2024-07-23 12:40:44 +0300

    Fix reload issue.

--- src/components/common/details/DetailsComponent.jsx
+++ src/components/common/details/DetailsComponent.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import Spinner from '../Spinner';
@@ -7,13 +7,27 @@ import Spinner from '../Spinner';
 import Owner from './OwnerComponent';
 import NotOwner from './NotOwnerComponent';
 
-import { selectItemById, selectUserFromCatalog } from '../../../slices/itemsSlice';
+import { getItems, selectItemById, selectItems, selectUserFromCatalog } from '../../../slices/itemsSlice';
+import { useEffect } from 'react';
 
 export default function Details() {
+    const dispatch = useDispatch();
+
     const { id } = useParams();
-    
+
     const item = useSelector(state => selectItemById(state, id));
+
     const user = useSelector(state => selectUserFromCatalog(state));
+
+    const items = useSelector(state => selectItems(state));
+
+    useEffect(() => {        
+        if(items.length === 0){
+            dispatch(getItems());
+        }
+        // eslint-disable-next-line
+    }, []);
+
 
     if (item) {
         const isOwner = item.owner === user?.id;
