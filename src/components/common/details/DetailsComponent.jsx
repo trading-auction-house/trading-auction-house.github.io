b6bb29cf0d8c8207166251e82427727b0c8d20e6
atanas-zahariev <atanaszaharieff@abv.bg>
2024-08-01 17:44:17 +0300

    Showing searching items details.

--- src/components/common/details/DetailsComponent.jsx
+++ src/components/common/details/DetailsComponent.jsx
@@ -1,25 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Spinner from '../Spinner';
 
 import Owner from './OwnerComponent';
 import NotOwner from './NotOwnerComponent';
 
-import { getItems, selectItemById, selectItems, selectUserFromCatalog } from '../../../slices/itemsSlice';
-import { useEffect } from 'react';
+import { getItems, selectItemById, selectItems, selectSearchArray, selectUserFromCatalog } from '../../../slices/itemsSlice';
+
+import { getSearch } from '../../../services/utility';
 
 export default function Details() {
     const dispatch = useDispatch();
 
     const { id } = useParams();
 
-    const item = useSelector(state => selectItemById(state, id));
+    const searchItems = useSelector(selectSearchArray) || getSearch();
+
+    const item = useSelector(state => selectItemById(state, id)) || searchItems.filter(item => item.id === id)[0];
 
     const user = useSelector(state => selectUserFromCatalog(state));
 
     const items = useSelector(state => selectItems(state));
+
 
     useEffect(() => {        
         if(items.length === 0){
